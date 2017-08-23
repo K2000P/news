b4ce5bfe483c2c19773085e9d48896f46706c823
K2000P <PKALRA99@YAHOO.COM>
2017-08-23 12:07:08 -0700

    Update news.php

--- news.php
+++ news.php
@@ -2,6 +2,6 @@
 		<?php
 				$url1="http://www.google.com";
 				$url='https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=5442ba94308445dab5609ac218fc7cc0';
-				echo '<h3>'.$url1+'</h3><br>'.'<h4>'.$url1.'</h4>';
+				echo '<h3>'.$url1.'</h3><br>'.'<h4>'.$url1.'</h4>';
 		?>
 
